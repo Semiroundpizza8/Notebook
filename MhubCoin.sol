864fdb80d8996701e0bf8401e701702ffb3ad4f2
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-04-21 14:24:16 -0500

    inished mhub coin

--- MhubCoin.sol
+++ MhubCoin.sol
@@ -1,3 +1,6 @@
+// see qmix.com
+// qmix checks your node, picks the most valuable utxos, and picks one for you
+
 pragma solidity ^0.4.18;
 
 contract MhubCoin {
@@ -6,11 +9,28 @@ contract MhubCoin {
     uint256 public maxSupply;
     address public owner;
 
+    // Events allow you to communicate events back to the front of the site.
+    // No logging, so you need to feed info to the front end. Takes up to 3 fields
+    event BuyIn(
+        address indexed _buyer,
+        uint256 _amount
+    );
+    event CashOut(
+        address indexed _reveiver,
+        uint256 _amount
+    );
+
     // Very memory efficient
     mapping (address => uint256) balances;
 
     // Modifier
-    modifier _onlyOwner
+    // Decorator, function must run this first to confirm its legit.
+    modifier _onlyOwner()
+    {
+        require(msg.sender == owner);
+        _; // says then do the rest of the function.
+        // _; signifies where the rest of the function goes within decorator. Anything before it is run before code executes, and anything after it is run after code executes.
+    }
     /*
     function constructor
     msg.sender gets information about the person executing the function.
@@ -76,5 +96,15 @@ contract MhubCoin {
         // Concurrency isn't a consideration, it comes down to whoever is picked up first by miners.
         require(currentSupply <= maxSupply);
         balances[msg.sender] += msg.value;
+        ByIn(msg.sender, msg.value);
+    }
+    // C20 is the standardized way... Openzeplin too
+    function cashOut(uint256 amount)
+    public
+    {
+        require(balance(msg.sender) >= amount);
+        balances[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+        CashOut(msg.sender, amount);
     }
 }
