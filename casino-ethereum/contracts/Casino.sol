b7df9f7ab117f793d7a95276bed56e0ac30c09d8
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-04-26 23:27:12 -0500

    Casino edits

--- casino-ethereum/contracts/Casino.sol
+++ casino-ethereum/contracts/Casino.sol
@@ -13,14 +13,16 @@ contract Casino {
     uint numberSelected;
   }
 
+  mapping (address => Player) public playerInfo;
+
+  function () public payable {}
+
   function checkPlayerExists(address _player) public constant returns(bool){
     for(uint i=0; i<players.length; i++){
       if(players[i] == _player) return true;
     }
     return false;
   }
-
-  mapping (address => Player) public playerInfo;
 
   constructor(uint _minimumBet) public {
     owner = msg.sender;
@@ -41,5 +43,35 @@ contract Casino {
     numberOfBets++;
     players.push(msg.sender);
     totalBet += msg.value;
+
+    if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
+  }
+
+  function generateNumberWinner() public {
+    uint numberGenerated = block.number % 10 + 1;
+    distributePrizes(numberGenerated);
+  }
+
+  function distributePrizes(uint numberGenerated) public {
+    address[100] memory winners;
+    uint count = 0;
+    for(uint i=0; i<players.length; i++) {
+      address playersAddress = players[i];
+      if(playerInfo[playersAddress].numberSelected == numberGenerated) {
+        winners[count] = playersAddress;
+        count++;
+      }
+    }
+
+    players.length = 0;
+
+    uint amountWon = totalBet / winners.length;
+
+    for(uint j=0; j<count; j++){
+      if(winners[j] != 0) {
+        winners[j].transfer(amountWon);
+      }
+    }
+
   }
 }
